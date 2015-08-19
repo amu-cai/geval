17844b59212eceb0e363091a2ba6bc617126f2c6
Filip Gralinski <filipg@amu.edu.pl>
2015-08-19 22:14:34 +0200

    init cntd.

--- src/Lib.hs
+++ src/Lib.hs
@@ -2,6 +2,24 @@ module Lib
     ( geval
     ) where
 
+import Data.Conduit
+import Data.Conduit.Combinators as CC
+import qualified Data.Conduit.Binary as CB
+import qualified Data.Conduit.Text as CT
+import Control.Monad.Trans.Resource
+import qualified Data.Conduit.List as CL
+import Data.Text
+import Data.Text.Read as TR
+
 geval :: String -> String -> IO (Double)
 geval expectedFilePath outFilePath = do
-  return $ 3.14
+  runResourceT $
+    CB.sourceFile expectedFilePath
+    $$ CT.decode CT.utf8
+    =$= CT.lines
+    =$= CL.map TR.double
+    =$= CC.map getValue
+    =$ CC.sum
+
+getValue :: Either String (Double, Text) -> Double
+getValue (Right (x, _)) = x
--- test/Spec.hs
+++ test/Spec.hs
@@ -1,2 +1,9 @@
+import Test.Hspec
+
+import Lib
+
 main :: IO ()
-main = putStrLn "Test suite not yet implemented"
+main = hspec $ do
+  describe "mean square error" $ do
+    it "simple test" $ do
+      geval "test/mse-simple/mse-simple/test-A/expected.tsv" "test/mse-simple/mse-simple-solution/test-A/out.tsv" `shouldReturn` 1.118033988749
